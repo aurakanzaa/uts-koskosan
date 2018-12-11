1163e5d143f1065385ca88da339a08b57ed0ad9f
andinovanprastya <andinovan@gmail.com>
2018-12-11 07:11:57 +0700

    location tahap 4 belum selesai

--- app/src/main/java/com/example/andinovanprastya/loginfirebase/location/LocationActivity.java
+++ app/src/main/java/com/example/andinovanprastya/loginfirebase/location/LocationActivity.java
@@ -1,6 +1,8 @@
 package com.example.andinovanprastya.loginfirebase.location;
 
 import android.Manifest;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
@@ -10,10 +12,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.example.andinovanprastya.loginfirebase.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
@@ -24,12 +30,19 @@ public class LocationActivity extends AppCompatActivity implements DapatkanAlama
     private static final int REQUEST_LOCATION_PERMISSION =1;
     private Location mLastLocation;
     private FusedLocationProviderClient mFusedLocationClient;
+    private ImageView mAndroidImageView;
+    private AnimatorSet mRotateAnim;
+    private boolean mTrackingLocation;
+    private LocationCallback mLocationCallback;
 
     @Override
     public void onTaskCompleted(String result){
-        //update UI dengan tampilan hasil alamat
-        mLocationTextView.setText(getString(R.string.alamat_text,
-                result, System.currentTimeMillis()));
+        if (mTrackingLocation){
+            //update UI dengan tampilan hasil alamat
+            mLocationTextView.setText(getString(R.string.alamat_text,
+                    result, System.currentTimeMillis()));
+        }
+
     }
 
     @Override
@@ -39,19 +52,41 @@ public class LocationActivity extends AppCompatActivity implements DapatkanAlama
 
         mLocationButton = (Button) findViewById(R.id.button_location);
         mLocationTextView = (TextView) findViewById(R.id.textview_location);
+        mAndroidImageView = (ImageView) findViewById(R.id.imageview_android);
+
+        //animasi
+        mRotateAnim = (AnimatorSet) AnimatorInflater.loadAnimator
+                (this, R.animator.rotate);
+        mRotateAnim.setTarget(mAndroidImageView);
 
         mLocationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getLocation();
+//                mulaiTrackingLokasi();
+                if (!mTrackingLocation){
+                    mulaiTrackingLokasi();
+                } else {
+                    stopTrackingLokasi();
+                }
             }
         });
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
+        mLocationCallback = new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult){
+                //jika tracking aktif, proses reverse geoode menjadi data alamat
+                if (mTrackingLocation){
+                    new DapatkanAlamatTask(LocationActivity.this, LocationActivity.this)
+                            .execute(locationResult.getLastLocation());
+                }
+            }
+        };
+
     }
 
-    private void getLocation() {
+    private void mulaiTrackingLokasi() {
         if (ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -59,34 +94,18 @@ public class LocationActivity extends AppCompatActivity implements DapatkanAlama
                             {Manifest.permission.ACCESS_FINE_LOCATION},
                     REQUEST_LOCATION_PERMISSION);
         } else {
-            Log.d("GETPERMISI", "getLocation: permissions granted");
-//            Log.d("GETPERMISI", "getLocation: permissions granted");
-            mFusedLocationClient.getLastLocation().addOnSuccessListener(
-                    new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            if (location != null) {
-                                // lakukan reverse geocode AsyncTask
-                                new DapatkanAlamatTask(LocationActivity.this, LocationActivity.this).execute(location);
-
-//                                mLastLocation = location;
-//                                mLocationTextView.setText(
-//                                        getString(R.string.location_text,
-//                                                mLastLocation.getLatitude(),
-//                                                mLastLocation.getLongitude(),
-//                                                mLastLocation.getTime())
-//                                );
-                            } else {
-                                mLocationTextView.setText("Lokasi tidak tersedia");
-                            }
-                        }
-                    }
-            );
+//            Log.d("GETPERMISI", "mulaiTrackingLokasi: permissions granted");
+//            Log.d("GETPERMISI", "mulaiTrackingLokasi: permissions granted");
+            mFusedLocationClient.requestLocationUpdates
+                    (getLocationRequest(), mLocationCallback, null);
+            //menampilkan text loading
+            mLocationTextView.setText(getString(R.string.alamat_text,
+                    "Sedang mencari alamat",
+                    System.currentTimeMillis()));
+            mTrackingLocation = true;
+            mLocationButton.setText("Stop Tracking Lokasi");
+            mRotateAnim.start();
         }
-        //menampilkan text loading
-        mLocationTextView.setText(getString(R.string.alamat_text,
-                "Sedang mencari alamat",
-                System.currentTimeMillis()));
     }
 
     @Override
@@ -98,7 +117,7 @@ public class LocationActivity extends AppCompatActivity implements DapatkanAlama
                 // jika tidak, tampilkan Toast
                 if (grantResult.length > 0
                         && grantResult[0] == PackageManager.PERMISSION_GRANTED){
-                    getLocation();
+                    mulaiTrackingLokasi();
                 } else {
                     Toast.makeText(this,
                             "Permission bapakknya gak dapet masbro, mesakke", Toast.LENGTH_SHORT).show();
@@ -106,5 +125,22 @@ public class LocationActivity extends AppCompatActivity implements DapatkanAlama
         }
     }
 
+    private void stopTrackingLokasi(){
+        if (mTrackingLocation){
+            mTrackingLocation = false;
+            mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+            mLocationButton.setText("Mulai Tracking Lokasi");
+            mLocationTextView.setText("Tracking sedang dihentikan");
+            mRotateAnim.end();
+        }
+    }
+
+    private LocationRequest getLocationRequest(){
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return locationRequest;
+    }
 
 }
